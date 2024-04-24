7ae21faae2128e33e985b69741e0602c2b758cd0
dishapurwar <dishapurwar679@gmail.com>
2024-04-24 23:58:12 +0530

    first commit

--- api/models/User.js
+++ api/models/User.js
@@ -1,22 +1,4 @@
 
-
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-
-// const UserSchema = new Schema({
-//     name: String,
-//     email: { type: String, unique: true },
-//     password: String,
-//     role: { type: String, enum: ['admin', 'user'], default: 'user' },
-//     userName: { type: String, unique: true, sparse: true, required: function () { return this.role === 'admin'; } },
-//     adminPassword: { type: String, required: function () { return this.role === 'admin'; } }
-// });
-
-// const UserModel = mongoose.model('User', UserSchema);
-
-// module.exports = UserModel;
-
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
